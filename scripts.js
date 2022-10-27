d535f4ff3cce0569013c3873ecb674ae79a4eb58
0lucus <63613363+0lucus@users.noreply.github.com>
2022-10-27 18:02:46 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
-      document.querySelector(".search").addEventListener("click", searchBook);
+document.querySelector(".search").addEventListener("click", searchBook);
 
-      function searchBook() {
-        const isbn = document.querySelector(".inputISBN").value.trim().split('-').join('');
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
-          .then(book => book.json())
-          .then(data => {
-            const details = data.items[0].volumeInfo;
-            document.querySelector(".thumbnail").setAttribute("src", details.imageLinks.smallThumbnail);
-            document.querySelector(".author").innerHTML = details.authors.join();
-            document.querySelector(".title").innerHTML = details.title;
-            document.querySelector(".description").innerHTML = data.items[0].searchInfo.textSnippet;
-          })
-          .catch(() => alert("Wpisałeś zły numer ISBN lub książka nie istnieje w bazie"));
+function searchBook() {
+  const isbn = document
+    .querySelector(".inputISBN")
+    .value.trim()
+    .split("-")
+    .join("");
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
+    .then((book) => book.json())
+    .then((data) => {
+      console.log(data);
+      const details = data.items[0].volumeInfo;
+
+      if (details.hasOwnProperty("imageLinks")) {
+        document
+          .querySelector(".thumbnail")
+          .setAttribute("src", details.imageLinks.smallThumbnail);
       }
+
+      document.querySelector(".author").innerHTML = details.authors.join();
+
+      document.querySelector(".title").innerHTML = details.title;
+
+      if (details.hasOwnProperty("searchInfo")) {
+        document.querySelector(".description").innerHTML =
+          data.items[0].searchInfo.textSnippet;
+      }
+    })
+    .catch(() =>
+      alert("Podana książka nie istnie w bazie lub podałeś zły numer ISBN")
+    );
+}
